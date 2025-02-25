de90edc62e8b4164c9c11e480a9c0e19f5cad9b2
oscar <19324475@brookes.ac.uk>
2025-02-25 15:04:50 +0000

    finish main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
-        Player player1 = new Player("ghodfh");
-        Player player2 = new Player("asfda");
-        Player player3 = new Player("fgsa");
+        Player player1 = new Player("a");
+        Player player2 = new Player("b");
+        Player player3 = new Player("c");
         Game game = new Game();
         game.addPlayer(player1);
         game.addPlayer(player2);
         game.addPlayer(player3);
 
-        game.playRound();
+        while (!game.gameOver()) {
+            game.playRound();
+            System.out.println();
+        }
     }
 }
