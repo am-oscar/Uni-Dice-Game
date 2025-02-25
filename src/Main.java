85db48ee30b5440f652ae5ec6d49d788ac4f0eb2
oscar <19324475@brookes.ac.uk>
2025-02-25 15:37:04 +0000

    test with 5 players

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,14 @@ public class Main {
         Player player1 = new Player("a");
         Player player2 = new Player("b");
         Player player3 = new Player("c");
+        Player player4 = new Player("d");
+        Player player5 = new Player("e");
         Game game = new Game();
         game.addPlayer(player1);
         game.addPlayer(player2);
         game.addPlayer(player3);
+        game.addPlayer(player4);
+        game.addPlayer(player5);
 
         while (!game.gameOver()) {
             game.playRound();
